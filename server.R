87d43cd4a63df0d5bc079359d759ccfb7e07fe29
unknown <needhamd@IW01MSP132.illuminationworksllc.com>
2015-05-30 22:59:36 -0400

    Updates to display

--- server.R
+++ server.R
@@ -5,6 +5,7 @@
 # June 2015
 library(shiny)
 library(igraph)
+library(stringr)
 get_color <- function(index) {
   color_list <- colors()
   color_string <- color_list[index]
@@ -22,6 +23,10 @@ shinyServer(function(input, output,session) {
     Life.Graph_input <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                       quote=input$quote)
     Life.Graph <- graph.data.frame(Life.Graph_input,directed=TRUE)
+    Life.Graph$Origin <- str_trim(Life.Graph$Origin)
+    Life.Graph$Objective <- str_trim(Life.Graph$Objective)
+    Life.Graph$Why <- str_trim(Life.Graph$Why)
+    Life.Graph$Day <- str_trim(Life.Graph$Day)
     why_list <- as.data.frame(unique(sort(Life.Graph_input$Why)))
     options <- c()
     for (index in why_list[,1]) { options = c(options,c(index,index))}
@@ -33,7 +38,7 @@ shinyServer(function(input, output,session) {
     updateSelectInput(session, 'Day', choices = options)    
     
     col_levels <- levels(Life.Graph_input$Origin)
-    V(Life.Graph)$size       <- 8
+    V(Life.Graph)$size       <- degree(Life.Graph)*2
     V(Life.Graph)$label.cex  <- 1
     V(Life.Graph)$label.dist <- 1
     V(Life.Graph)$color      <- get_color(which(col_levels %in% Life.Graph_input[V(Life.Graph),2]))
@@ -49,9 +54,15 @@ shinyServer(function(input, output,session) {
     
     Life.Graph_input <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                       quote=input$quote)
+    Life.Graph_input$Why <- str_trim(Life.Graph_input$Why)    
+    
     point_of_origin = 0
-    Life.Graph <- graph.data.frame(Life.Graph_input[Life.Graph_input$Why==input$thread,],directed=TRUE)    
-    V(Life.Graph)$size       <- 10
+    Life.Graph <- graph.data.frame(Life.Graph_input[Life.Graph_input$Why==input$thread,],directed=TRUE) 
+    Life.Graph$Origin <- str_trim(Life.Graph$Origin)
+    Life.Graph$Objective <- str_trim(Life.Graph$Objective)
+    Life.Graph$Why <- str_trim(Life.Graph$Why)
+    Life.Graph$Day <- str_trim(Life.Graph$Day)
+    V(Life.Graph)$size       <- degree(Life.Graph)*1.5
     V(Life.Graph)$color      <- "green"
     V(Life.Graph)$label.cex  <- 1
     V(Life.Graph)$label.dist <- 1
@@ -67,9 +78,15 @@ shinyServer(function(input, output,session) {
     
     Life.Graph_input <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                                  quote=input$quote)
+    Life.Graph_input$Day <- str_trim(Life.Graph_input$Day)
+
     point_of_origin = 0
-    Life.Graph <- graph.data.frame(Life.Graph_input[Life.Graph_input$Day==input$Day,],directed=TRUE)    
-    V(Life.Graph)$size       <- 10
+    Life.Graph <- graph.data.frame(Life.Graph_input[Life.Graph_input$Day==input$Day,],directed=TRUE) 
+    Life.Graph$Origin <- str_trim(Life.Graph$Origin)
+    Life.Graph$Objective <- str_trim(Life.Graph$Objective)
+    Life.Graph$Why <- str_trim(Life.Graph$Why)
+    Life.Graph$Day <- str_trim(Life.Graph$Day)
+    V(Life.Graph)$size       <- degree(Life.Graph)*1.2
     V(Life.Graph)$color      <- "green"
     V(Life.Graph)$label.cex  <- 1
     V(Life.Graph)$label.dist <- 1
